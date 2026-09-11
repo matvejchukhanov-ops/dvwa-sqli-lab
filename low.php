b4126339b97b04fce3b67c77920159d17b81053e
matvejchukhanov-ops <matvejchukhanov@gmail.com>
2026-09-11 15:48:10 +0300

    Refactor database query handling in low.php

--- low.php
+++ low.php
@@ -1,33 +1,17 @@
 <?php
-\
-if( isset( $_GET[ 'Submit' ] ) ) {
-// Get input
-$id = $_GET[ 'id' ];
-\
-// Check database
-$getid = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-$result = mysqli_query($GLOBALS["___mysqli_ston"], $getid ); // Removed 'or die'
-Требования для лабораторной работы
-1а. или сервер с установленным программным обеспчением 2. Редактор
-кода для языков PHP и выбранного Вами языка для написания программы
-перебора пароля для формы аутентификации. 3. Облачная версия
- или установленная на компьютере
-\
-// Get results
-$num = @mysqli_num_rows( $result ); // The '@' character suppresses errors
-if( $num > 0 ) {
-// Feedback for end user
-$html .= '<pre>User ID exists in the database.</pre>';
+$conn = new mysqli("localhost", "dvwa_user", "dvwa_pass", "dvwa");
+
+if (isset($_GET['Submit'])) {
+    $id = $_GET['id'];
+
+    $getid  = "SELECT first_name, last_name FROM users WHERE user_id = '" . $id . "'";
+    $result = mysqli_query($conn, $getid);
+
+    $num = mysqli_num_rows($result);
+    if ($num > 0) {
+        echo "User ID exists in the database.";
+    } else {
+        echo "User ID is MISSING from the database.";
+    }
 }
-else {
-// User wasn't found, so the page wasn't!
-header( $_SERVER[ 'SERVER_PROTOCOL' ] . ' 404 Not Found' );
-\
-// Feedback for end user
-$html .= '<pre>User ID is MISSING from the database.</pre>';
-}
-\
-((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $_
-}
-\
 ?>
